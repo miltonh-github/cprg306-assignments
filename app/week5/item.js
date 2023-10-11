f7d3d08b42fd03f390ccac0920cc980cbe9b629c
miltonh-github <119451755+miltonh-github@users.noreply.github.com>
2023-10-10 21:16:38 -0600

    Week5: json load to Array

--- app/week5/item.js
+++ app/week5/item.js
@@ -3,12 +3,12 @@
 import Link from "next/link";
 import ItemList from "app/week3/item-list";
 
-export default function Item({name, quantity, category}) {
+export default function Item({item}) {
     return (
         <li className="border border-yellow-800 bg-yellow-400 w-96 m-4 p-2">
-            <h3 className="text-xl font-bold">{name}</h3>
-            <p>Quantity: {quantity}</p>
-            <p>Category: {category}</p>
+            <h3 className="text-xl font-bold">{item.name}</h3>
+            <p>Quantity: {item.quantity}</p>
+            <p>Category: {item.category}</p>
         </li>
 
     )
