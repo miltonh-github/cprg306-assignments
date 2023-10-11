b024daf653ada58edd6c2acb26a38bb03484b14f
miltonh-github <119451755+miltonh-github@users.noreply.github.com>
2023-10-10 21:41:45 -0600

    Complete Week5 sorting buttons

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -10,11 +10,32 @@ export default function ItemList() {
       // map the JSON data to an array
     let itemArray = itemsData.map((item) => ({ ...item }));
 
+    itemArray = itemArray.sort((a, b) => {
+      if (isNaN(parseInt(a[sortBy]))) {
+        // sort alphabetically
+        let nameA = a[sortBy].toUpperCase();
+        let nameB = b[sortBy].toUpperCase();
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      } else {
+        return a[sortBy] - b[sortBy];
+      }
+    });
+
       return (
-        <section className="grid grid-cols-3 gap-5 p-5">
-          {itemArray.map((item) => (
-            <Item item={item} key={item.id}/>
-          ))}
-        </section>
+        <>
+          <button class={sortBy === "name" ? "bg-yellow-500 p-1 m-2 w-28" : "bg-yellow-400 p-1 m-2 w-28"} onClick={(e) => setSortBy(e.target.value)} value="name">Name</button>
+          <button class={sortBy === "category" ? "bg-yellow-500 p-1 m-2 w-28" : "bg-yellow-400 p-1 m-2 w-28"} onClick={(e) => setSortBy(e.target.value)} value="category">Category</button>
+          <section className="grid grid-cols-3 gap-5 p-5">
+            {itemArray.map((item) => (
+              <Item item={item} key={item.id}/>
+            ))}
+          </section>
+        </>
       )
 }
