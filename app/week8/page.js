bf1cda1142c456845c3f4788c68d0904c16b919f
miltonh-github <119451755+miltonh-github@users.noreply.github.com>
2023-11-01 23:21:09 -0600

    week8 login complete

---
+++ app/week8/page.js
@@ -0,0 +1,54 @@
+"use client";
+
+import { useUserAuth } from "./_utils/auth-context";
+import Link from "next/link";
+
+export default function Page() {
+const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+async function handleSignIn() {
+    try {
+        await gitHubSignIn();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function handleSignOut() {
+    try {
+        await firebaseSignOut();
+        alert("Signed out!")
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+return (
+    <div className="block mx-auto w-64 my-64">
+        <h1 className="text-center">Login via Github</h1>
+        {user ? (
+            <p>
+                <img src={user.photoURL} className="w-8 h-8" />
+                Welcome, {user.displayName} {user.email}
+                <button
+                className="text-lg block mx-auto px-6 hover:underline bg-purple-300 rounded-md"
+                onClick={handleSignOut}>Sign Out</button>
+                <section className="text-lg flex flex-col items-center p-12 bg-purple-300 mt-6">
+                <h1 className="text-xl">PAGE LINKS</h1>
+                <br/>
+                <Link href="/week8/shopping-list" class="hover:text-sky-700 text-purple-950">SHOPPING LIST</Link>
+            </section>
+            </p>
+            ) : (
+                <button
+                    className="text-lg block mx-auto px-6 hover:underline bg-purple-300 rounded-md"
+                    onClick={handleSignIn}
+                >
+                    Sign in
+                </button>
+            )}
+    </div>
+    
+)
+}
+
---
+++ app/week8/shopping-list/page.js
@@ -0,0 +1,52 @@
+"use client";
+
+// WEEK 8
+
+import { useState } from "react";
+import { useUserAuth } from "../_utils/auth-context"
+import ItemList from "./item-list"  
+import NewItem from "./new-item"
+import itemsData from "./items.json";
+import MealIdeas from "./meal-ideas.js";
+
+export default function Home() {
+  const [items, addNewItem] = useState(itemsData.map((item) => ({ ...item })));
+  const [selectedItemName, updateItemState] = useState(null);
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  // problem
+  const handleItemSelect = (item) => {
+    //Two-step itemName cleaning
+    // First remove all emojis, then remove commas
+    let itemName = item.name.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF]|",")/g, '');;
+    itemName = itemName.split(', ')[0];
+    // alert(itemName); //DEBUG
+    updateItemState(itemName);
+  }
+
+  const handleAddItem = (item) => {
+    addNewItem([...items, item]);
+  };
+    return (
+      <>
+        {user ? (<main className="flex flex-col p-24">
+          <h1 className="text-2xl font-bold m-2 text-yellow-400 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">My Shopping List</h1>
+          <ul>
+          <div className="flex flex-row content-around">
+              <NewItem onAddItem={handleAddItem}/>
+              <MealIdeas ingredient={selectedItemName}/>
+          </div>
+          <ItemList items={items} onItemSelect={handleItemSelect}/>
+
+          </ul>
+          
+        </main>) : (
+          <p>Must be logged in to access page contents.</p>
+          
+        )}
+        
+        
+      </>
+    )
+  }
+  
